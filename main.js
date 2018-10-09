cecb59a12fe4c980f9a65675c3aefb2dbfca89fd
Maxence Fourrier <maxence.fourrier@epitech.eu>
2018-10-09 16:05:41 +0200

    Added module mysql init

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 var express = require('express');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
+var mysql = require('mysql');
+var body = require('body-parser');
 
 var app = express();
 
+app.use(morgan('combined'))
+app.use(express.static(__dirname + '/public'))
+app.use(favicon(__dirname + '/public/favicon.ico'))
+/*
+app.use(function(req, res){
+	res.send('Hello');
+});*/
+
 app.get('/', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
+	res.render('index.ejs');
 });
 
-app.get('/sous-sol', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-});
-
-app.get('/etage/1/chambre', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.send('Hé ho, c\'est privé ici !');
-});
-
-
-app.use(function(req, res, next){
-	    res.setHeader('Content-Type', 'text/plain');
-	    res.status(404).send('Page introuvable !');
+app.get('/etage/:etagenum/chambre', function(req, res) {
+	res.render('tmp.ejs', {
+		etage : req.params.etagenum
 	});
+});
+
+app.use(function(req, res, next) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.status(404).send('Page introuvable !');
+});
 
 app.listen(8080);
