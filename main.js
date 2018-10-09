102b46fcdf3398273faa87f66a05078c563be3d5
Maxence Fourrier <maxence.fourrier@epitech.eu>
2018-10-09 17:55:54 +0200

    get params from html to js

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 var express = require('express');
-var morgan = require('morgan');
-var favicon = require('serve-favicon');
-var mysql = require('mysql');
-var body = require('body-parser');
+//var morgan = require('morgan');
+//var favicon = require('serve-favicon');
+//var mysql = require('mysql');
+//var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(morgan('combined'))
-app.use(express.static(__dirname + '/public'))
-app.use(favicon(__dirname + '/public/favicon.ico'))
+//app.use(morgan('combined'))
+//app.use(express.static(__dirname + '/public'))
+//app.use(favicon(__dirname + '/public/favicon.ico'))
+//var urlencodedParser = bodyParser.urlencoded({ extended: true });
 /*
  * app.use(function(req, res){ res.send('Hello'); });
  */
 
+/*
 app.get('/', function(req, res) {
 	res.render('index.ejs');
-	
+	var username = req.query.username;
+	var password = req.query.password;
+	if (typeof username !== 'undefined' && username
+			&& typeof password !== 'undefined' && password) {
+		console.log('Trying Login.');
+		console.log('username = ' + username);
+		console.log('password = ' + password);
+	}
+});*/
+
+//GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage');
 });
 
-app.post('/stack',function(req,res) {
-	var tmp = req.query.stack0;
-	var tmp1 = req.query.stack1;
-
-	console.log(tmp);  // stackoverflow0
-	console.log(tmp1);  // stackoverflow1
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
 });
 
-app.get('/index1', function(req, res) {
+
+app.get('/index', function(req, res) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.send('hello');
 });
