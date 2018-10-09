54efdc0b2cb518797dbb973dcce0e0a641a05ac3
Maxence Fourrier <maxence.fourrier@epitech.eu>
2018-10-09 17:02:49 +0200

    Get text from html DEV

--- main.js
+++ main.js
@@ -10,12 +10,25 @@ app.use(morgan('combined'))
 app.use(express.static(__dirname + '/public'))
 app.use(favicon(__dirname + '/public/favicon.ico'))
 /*
-app.use(function(req, res){
-	res.send('Hello');
-});*/
+ * app.use(function(req, res){ res.send('Hello'); });
+ */
 
 app.get('/', function(req, res) {
 	res.render('index.ejs');
+	
+});
+
+app.post('/stack',function(req,res) {
+	var tmp = req.query.stack0;
+	var tmp1 = req.query.stack1;
+
+	console.log(tmp);  // stackoverflow0
+	console.log(tmp1);  // stackoverflow1
+});
+
+app.get('/index1', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.send('hello');
 });
 
 app.get('/etage/:etagenum/chambre', function(req, res) {
